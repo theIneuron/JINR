2368322f84e46e655ecd1fc77629c1ac44f7828e
theIneuron <boazidhonsultonov@gmail.com>
2024-04-22 02:18:47 +0300

    Update trajectory.py

--- trajectory.py
+++ trajectory.py
@@ -1,108 +1,12 @@
-#privet
-
-#import math
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
-# ec2 = 1.44
-# a = 0.55
-# b = float((np.pi/math.sqrt(3))*a)
-# Z1 = 40
-# Z2 = 60
-# A1 = 90
-# A2 = 144
-# R1 = 1.28*(A1**(1/3)) - 0.76 + 0.8*(A1**(-1/3))
-# R2 = 1.28*(A2**(1/3)) - 0.76 + 0.8*(A2**(-1/3))
-# C1 = R1*(1 - (b/R1)**2)
-# C2 = R2*(1 - (b/R2)**2)
-# RC = (C1*C2)/(C1+C2)
-# #s = r - C1 - C2
-# gamma0 = 0.9517
-# k = 1.7826
-# N1 = A1 - Z1
-# N2 = A2 - Z2
-# A = ( (N1+N2) - (Z1+Z2) )/(A1+A2)
-# gamma_const = gamma0*(1 - k*A)
-# #s = r - C1 - C2
-# #e = s/b
-# e = 1.21
-# e2 = 1.25
-# #r1 = b*e1 + C1 + C2
-# #r2 = b*e2 + C1 + C2
-# rmassa = A1 * A2 / (A1 + A2)
-#
-# def gamma(R0):
-#     return -0.67754 + 223358.47648 * math.exp(-0.90292 * R0)
-#
-#
-#
-# def calculate_next_R(R0, V, dt, gamma, rmassa, dUdR):
-#     return R0 - V * dt**2 - (gamma(R0) * V / rmassa) * dt**2 - (dUdR * rmassa) * dt**2
-#
-#
-#
-# def U_n(r):
-#     if r < (1.2511 * b + (C1 + C2)):
-#         un = 4.0 * math.pi * gamma_const * b * RC * (
-#                     -((-2.54 + (-C1 - C2 + r) / b) / b) - (0.2556 * (-2.54 + (-C1 - C2 + r) / b) ** 2) / b)
-#         dUn = 4 * math.pi * gamma_const * b * RC * (
-#                     -((-2.54 + (-C1 - C2 + r) / b) / b) - (0.2556 * (-2.54 + (-C1 - C2 + r) / b) ** 2) / b)
-#     else:
-#         un = 4.0 * math.pi * gamma_const * b * RC * (4.58267 * math.exp(-((1.33333 * (-C1 - C2 + r)) / b))) / b
-#         dUn = 4 * math.pi * gamma_const * b * RC * (4.58267 * math.exp(-((1.33333 * (-C1 - C2 + r)) / b))) / b
-#     return un, dUn
-#
-# def Coul(r, Z1, Z2):
-#     U_c = ec2 * Z1 * Z2 / r
-#     dU_c = -ec2 * Z1 * Z2 / (r ** 2)
-#     return U_c, dU_c
-#
-# def calculate_trajectory():
-#     V0 = 0.0
-#     Ecm = 250.0
-#     dt = 0.05
-#     rmax = 20
-#     R0 = rmax
-#     dmr = 0.0
-#     Ekin = Ecm
-#     times = []
-#     positions = []
-#
-#     Rt = R0
-#     V = V0
-#
-#     for itr in range(201):
-#         t = itr * dt
-#         times.append(t)
-#         positions.append(Rt)
-#         un , dUn = U_n(Rt)
-#         U_c, dU_c = Coul(Rt, Z1, Z2)
-#         U = un + U_c
-#         dUdR = dUn + dU_c
-#
-#         Rt = calculate_next_R(R0, V, dt, gamma, rmassa, dUdR)
-#
-#     return times, positions
-#
-# times, positions = calculate_trajectory()
-#
-# plt.plot(times, positions)
-# plt.xlabel("t")
-# plt.ylabel("R(t)")
-# plt.title("Траектория")
-# plt.show()
-
-
-
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
-ec2 = 1.44
-a = 0.55
-b = float((np.pi/math.sqrt(3))*a)
-Z1 = 40
+ec2 = 1.44 # charge electron e^2
+a = 0.55 # const
+b = float((np.pi/math.sqrt(3))*a) # const
+Z1 = 40 
 Z2 = 60
 A1 = 90
 A2 = 144
